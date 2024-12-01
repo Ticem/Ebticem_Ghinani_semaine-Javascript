996046b4046d9ea61697b23d16585f4353f667ab
Ticem <ebticem.ghinani@edu.devinci.fr>
2024-12-01 13:59:34 +0100

    Add files via upload

---
+++ -Site Minimaliste-/Java de la page 1.js
@@ -0,0 +1,15 @@
+<script src="index.html"></script>
+
+// dark mode
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+function menu() {
+    var element = document.body;
+    element.classList.toggle
+    visibility 
+}
+
+
